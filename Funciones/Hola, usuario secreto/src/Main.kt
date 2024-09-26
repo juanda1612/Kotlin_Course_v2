93c62e3b9b7afc24cb0de089a2d81e7845547956
juanda1612 <juan.perez77@educa.madrid.org>
2024-09-26 13:07:46 +0200

    a

--- Funciones/Hola, usuario secreto/src/Main.kt
+++ Funciones/Hola, usuario secreto/src/Main.kt
@@ -1,3 +1,22 @@
 fun main() {
-    // Write your solution here
+    println("Nombre")
+    val nombre = readln()
+
+    println("¿Quieres ocultar tu nombre?")
+    val respuesta = readln()
+
+    if (respuesta.equals("si", ignoreCase = true)) {
+        println(saludos())
+    }else {
+        println(saludos(nombre))
+    }
 }
+
+fun saludos(nombre : String = "usuario secreto") : String {
+    return if (nombre == "usuario secreto"){
+        "Hola, usuario secreto"
+    }else {
+        "Hola, $nombre"
+    }
+}
+
